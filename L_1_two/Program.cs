9abe4c3328fe458b76eb50efbfd18dff66917ba3
Yana <yana.skrepchuk@yandex.ru>
2022-12-11 13:09:17 +0500

    Корректировка 3 кода

--- L_1_two/Program.cs
+++ L_1_two/Program.cs
@@ -1,4 +1,12 @@
-﻿int numA = 7;
-int numB = 5;
+﻿// int numA = 7;
+// int numB = 5;
+// int res = numA + numB;
+// Console.WriteLine(res);
+
+
+int numA = new Random().Next(2, 20);
+Console.WriteLine(numA);
+int numB = new Random().Next(2, 20);
+Console.WriteLine(numB);
 int res = numA + numB;
 Console.WriteLine(res);
